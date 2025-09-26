83b913717cda3124a74e12764fae3df836d1911d
PrimeSafar <dleerdleer515@gmail.com>
2025-09-26 19:41:19 +0300

    fix pending problem in react

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,24 @@ import "./App.css";
 import Arraypush from "./arrayPush";
 import ShoppingCart from "./arrayProblem";
 import { useState } from "react";
+import RequestTracker from "./PendingProblem";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
-  function handleIncClick() {
-    setCount((c)=>{
-      return c+1
-    });
-    setCount((c)=>{
-      return c+3
-    });
-  }
+  // function handleIncClick() {
+  //   setCount((c)=>{
+  //     return c+1
+  //   });
+  //   setCount((c)=>{
+  //     return c+3
+  //   });
+  // }
   return (
     <>
-      <h1>the count is : {count}</h1>
-      <button onClick={handleIncClick}>inc count </button>
+      {/* <h1>the count is : {count}</h1>
+      <button onClick={handleIncClick}>inc count </button> */}
+      <RequestTracker />
     </>
   );
 }
