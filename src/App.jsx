bfbbe49fd90bfa749fbeaa6c9916b073716146a3
PrimeSafar <dleerdleer515@gmail.com>
2025-09-26 17:37:23 +0300

    use state more than in code

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 // import SolScoreboard from './solFormChallenge'
-import Arraypush from './arrayPush'
-import ShoppingCart from './arrayProblem'
+import Arraypush from "./arrayPush";
+import ShoppingCart from "./arrayProblem";
+import { useState } from "react";
+
 function App() {
+  const [count, setCount] = useState(0);
+
+  function handleIncClick() {
+    setCount((c)=>{
+      return c+1
+    });
+    setCount((c)=>{
+      return c+3
+    });
+  }
   return (
-    <ShoppingCart />
-  )
+    <>
+      <h1>the count is : {count}</h1>
+      <button onClick={handleIncClick}>inc count </button>
+    </>
+  );
 }
 
-export default App
+export default App;
